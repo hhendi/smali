0329a6c889606ac1308d1d9f62384fa412dc6085
hhendi <34290542+hhendi@users.noreply.github.com>
2022-10-14 22:41:34 +0700

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -4,13 +4,10 @@ cd ~ || exit
 apt update
 apt upgrade -y
 apt install -y proot-distro figlet wget git make clang
-
-figlet Setting up env
 proot-distro install ubuntu-20.04
 wget -q https://github.com/hhendi/smali/blob/dev/android/setup2.sh?inline=false -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
 chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
 proot-distro login ubuntu-20.04 -- ./setup2.sh
-
 echo Creating convenience scripts
 echo "#!/bin/sh" > smalipatcher-shell
 echo "proot-distro login ubuntu-20.04" >> smalipatcher-shell
