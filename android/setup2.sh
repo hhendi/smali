f2348d2689670f35a71ea015918dbdce5380f62b
hhendi <34290542+hhendi@users.noreply.github.com>
2022-10-14 22:18:40 +0700

    Update setup2.sh

--- android/setup2.sh
+++ android/setup2.sh
@@ -9,29 +9,36 @@ chmod a+x dotnet-install.sh
 figlet Fetching patcher
 if [ "$(uname -m)" = "aarch64" ]
 then
-    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/master/download?job=android -O tmp.zip
+    wget -q https://github.com/hhendi/smali/tree/dev/SmaliPatcher/-/jobs/artifacts/master/download?job=android -O tmp.zip
 elif [ "$(uname -m)" = "arm64" ]
 then
-    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/master/download?job=android -O tmp.zip
+    wget -q https://github.com/hhendi/smali/tree/dev/SmaliPatcher/-/jobs/artifacts/master/download?job=android -O tmp.zip
 elif [ "$(uname -m)" = "armv7l" ]
 then
-    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/master/download?job=android-arm -O tmp.zip
+    wget -q https://github.com/hhendi/smali/tree/dev/SmaliPatcher/-/jobs/artifacts/master/download?job=android-arm -O tmp.zip
 elif [ "$(uname -m)" = "arm" ]
 then
-    wget -q https://gitlab.com/JFronny/smalipatcher/-/jobs/artifacts/master/download?job=android-arm -O tmp.zip
+    wget -q https://github.com/hhendi/smali/tree/dev/SmaliPatcher/-/jobs/artifacts/master/download?job=android-arm -O tmp.zip
 fi
 unzip tmp.zip
 rm tmp.zip
 figlet Building vdexExtractor
-git clone https://github.com/anestisb/vdexExtractor vdx
+git clone https://github.com/hhendi/vdex vdx
 cd vdx
 ./make.sh
 cp bin/vdexExtractor ..
 cd ..
 rm -rf vdx
+figlet downgrade openssl
+wget http://ports.ubuntu.com/ubuntu-ports/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_arm64.deb
+wget http://ports.ubuntu.com/ubuntu-ports/pool/main/o/openssl/openssl_1.1.1f-1ubuntu2.16_arm64.deb
+wget http://ports.ubuntu.com/ubuntu-ports/pool/main/o/openssl/libssl-dev_1.1.1f-1ubuntu2.16_arm64.deb
+dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_arm64.deb
+dpkg -i libssl-dev_1.1.1f-1ubuntu2.16_arm64.deb
+dpkg -i openssl_1.1.1f-1ubuntu2.16_arm64.deb
 echo Fetching scripts
-wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/cp_framework.sh?inline=false -O cp_framework.sh
+wget -q https://github.com/hhendi/smali/blob/dev/android/cp_framework.sh?inline=false -O cp_framework.sh
 chmod a+x cp_framework.sh
-wget -q https://gitlab.com/JFronny/smalipatcher/-/raw/master/android/smalipatcher.sh?inline=false -O smalipatcher.sh
+wget -q https://github.com/hhendi/smali/blob/dev/android/smalipatcher.sh?inline=false -O smalipatcher.sh
 chmod a+x smalipatcher.sh
 echo Env setup complete
