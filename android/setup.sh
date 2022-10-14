efc8dfd164675ada8bb099b95978657d2b6d9eef
hhendi <34290542+hhendi@users.noreply.github.com>
2022-10-14 23:51:00 +0700

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -5,7 +5,7 @@ apt update
 apt upgrade -y
 apt install -y proot-distro figlet wget git make clang
 proot-distro install ubuntu-20.04
-wget -q https://github.com/hhendi/smali/blob/dev/android/setup2.sh -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
+wget -q https://raw.githubusercontent.com/hhendi/smali/dev/android/setup2.sh -O $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
 chmod a+x $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu-20.04/root/setup2.sh
 proot-distro login ubuntu-20.04 -- ./setup2.sh
 echo Creating convenience scripts
